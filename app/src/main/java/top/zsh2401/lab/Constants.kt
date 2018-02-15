30d49a6b0ab3e1d64da343f501f255539c0a9d11
zsh2401 <zsh2401@163.com>
2018-02-16 06:23:01 +0800

    初步完成一些基本设计,通宵编程真的皮,睡觉了!

---
+++ app/src/main/java/top/zsh2401/lab/Constants.kt
@@ -0,0 +1,7 @@
+package top.zsh2401.lab
+
+/**
+ * Created by zsh24 on 02/16/2018.
+ */
+val DEVELOPER_MAIL = "zsh2401@163.com"
+val OPENSOURCE_URL = "http://github.com/zsh2401/2401slab"
--- app/src/main/java/top/zsh2401/lab/ui/viewitem/AboutLayout.kt
+++ app/src/main/java/top/zsh2401/lab/ui/viewitem/AboutLayout.kt
@@ -1,5 +1,6 @@
 package top.zsh2401.lab.ui.viewitem
 
+import android.annotation.SuppressLint
 import android.app.Activity
 import android.content.Context
 import android.content.Intent
@@ -20,7 +21,7 @@ class AboutLayout(context: Context):LinearLayout(context),View.OnClickListener {
             R.id.ll_card_about_1_feedback->{
                 val intent = Intent()
                 intent.action = Intent.ACTION_SENDTO
-                intent.data = (Uri.parse("mailto:zsh2401@163.com"))
+                intent.data = (Uri.parse("mailto:$DEVELOPER_MAIL"))
                 intent.putExtra(Intent.EXTRA_SUBJECT,App.current.resources.getString(R.string.feedback_title))
                 try{
                     App.current.startActivity(intent)
@@ -29,8 +30,14 @@ class AboutLayout(context: Context):LinearLayout(context),View.OnClickListener {
                             .setAction("ok",null).show()
                 }
             }
+            R.id.ll_card_about_2_os->{
+                val intent = Intent()
+                intent.data = Uri.parse(OPENSOURCE_URL)
+                intent.action = Intent.ACTION_VIEW
+                App.current.startActivity(intent)
+            }
             else->{
-                Snackbar.make(this,"sss",Snackbar.LENGTH_LONG)
+                Snackbar.make(this,"Here we go~",Snackbar.LENGTH_LONG)
                 .setAction("ok",null).show()}
         }
     }
@@ -40,6 +47,7 @@ class AboutLayout(context: Context):LinearLayout(context),View.OnClickListener {
         findViewById<LinearLayout>(R.id.ll_card_about_1_donate).setOnClickListener(this)
         findViewById<LinearLayout>(R.id.ll_card_about_1_feedback).setOnClickListener(this)
         findViewById<LinearLayout>(R.id.ll_card_about_2_os).setOnClickListener(this)
-        findViewById<TextView>(R.id.text_view_version).text = App.current.getString(R.string.about_version_name) + " " + getVersionName()
+        findViewById<TextView>(R.id.text_view_version).text =
+                App.current.getString(R.string.about_version_name) + " " + getVersionName()
     }
 }
